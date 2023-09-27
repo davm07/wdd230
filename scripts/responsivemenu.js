62124e1c1051d9a36d861d5be30e307f0789d208
Diego <diegovasquezmejia@gmail.com>
2023-09-27 22:49:17 +0200

    Update responsive menu and some font sizes

--- scripts/responsivemenu.js
+++ scripts/responsivemenu.js
@@ -5,3 +5,14 @@ hamButton.addEventListener("click", () => {
     navigation.classList.toggle("open");
     hamButton.classList.toggle("open");
 });
+
+//Gabriel Ferrin help with this code
+const observer = new ResizeObserver(entries => {
+    if (entries[0].contentRect.width >= 480) {
+        if (hamButton.classList.contains("open")) {
+            navigation.classList.toggle("open");
+            hamButton.classList.toggle("open");
+        }
+    }
+    });
+observer.observe(document.body);
