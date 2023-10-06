7cebb3f713e9902d63a59ba32d50e83e35eba0fc
Diego <diegovasquezmejia@gmail.com>
2023-10-06 02:46:55 +0200

    Finish all views

--- chamber/scripts/responsivemenu.js
+++ chamber/scripts/responsivemenu.js
@@ -6,13 +6,13 @@ hamButton.addEventListener("click", () => {
     hamButton.classList.toggle("open");
 });
 
-//Gabriel Ferrin help with this code
-const observer = new ResizeObserver(entries => {
-    if (entries[0].contentRect.width >= 480) {
-        if (hamButton.classList.contains("open")) {
-            navigation.classList.toggle("open");
-            hamButton.classList.toggle("open");
-        }
-    }
-    });
-observer.observe(document.body);
+// Gabriel Ferrin help with this code
+// const observer = new ResizeObserver(entries => {
+//     if (entries[0].contentRect.width >= 480) {
+//         if (hamButton.classList.contains("open")) {
+//             navigation.classList.toggle("open");
+//             hamButton.classList.toggle("open");
+//         }
+//     }
+//     });
+// observer.observe(document.body);
