053199178f84b99749c9cf849ceeb29ad0dd7acc
Diego <diegovasquezmejia@gmail.com>
2023-10-24 00:20:24 +0200

    Finish Couse Home Page visits counter

---
+++ scripts/updatevisits.js
@@ -0,0 +1,13 @@
+const visitsDisplay = document.querySelector('#page-visits');
+
+let visitsNum = Number(window.localStorage.getItem("visitsNum-ls")) || 0;
+
+if (visitsNum !== 0) {
+    visitsDisplay.textContent = visitsNum;
+} else {
+    visitsDisplay.textContent = `Welcome! This is your first visit! 🤓`;
+}
+
+visitsNum++;
+
+localStorage.setItem("visitsNum-ls", visitsNum);
