cd6d21dacdf716377eac7dea75ce355333d8cb32
Diego <diegovasquezmejia@gmail.com>
2023-11-04 21:49:55 +0100

    Finish form and confirmation page

--- chamber/scripts/timestamp.js
+++ chamber/scripts/timestamp.js
@@ -2,6 +2,5 @@ const timeStampField = document.querySelector("#timestamp");
 
 const date = new Date();
 const timeStampFormatted = date.toLocaleString();
-console.log(timeStampFormatted);
 
 timeStampField.value = timeStampFormatted;
