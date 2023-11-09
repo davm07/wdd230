8ff3365f20008db9299d7b04948ab7b0dbc5d11a
Diego <diegovasquezmejia@gmail.com>
2023-11-09 21:46:38 +0100

    Add directory page to the chamber

--- scripts/links.js
+++ scripts/links.js
@@ -6,7 +6,6 @@ async function getLinks() {
     const response = await fetch(linksURL);
     if (response.ok) {
         const data = await response.json();
-        // console.log(data.lessons);
         displayLinks(data.lessons);
     }
 }
@@ -18,17 +17,14 @@ const displayLinks = (weeks) => {
         listItem.setAttribute("class", "items");
         const innerLinks = week.links;
         innerLinks.forEach((link) => {
-            console.log(link);
             const listTag = document.createElement("a");
             listTag.setAttribute("href", link.url);
             listTag.setAttribute("target", "_blank");
             listTag.innerText = `${link.title}`;
             listItem.appendChild(listTag);
 
-            // index != innerLinks.length - 1
             if (link != innerLinks.at(-1)) {
-                // Check if it's not the last link
-                listItem.appendChild(document.createTextNode(" | ")); // Append the pipe separator
+                listItem.appendChild(document.createTextNode(" | "));
             }
         })
         linksList.appendChild(listItem);
