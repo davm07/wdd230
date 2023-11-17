ff9cf5c39de547662c358cea636d84c1882c0264
Diego <diegovasquezmejia@gmail.com>
2023-11-17 21:42:00 +0100

    Finish weather section in homepage

---
+++ chamber/scripts/banner.js
@@ -0,0 +1,19 @@
+const banner = document.querySelector("#banner");
+const button = document.querySelector("#close-banner");
+
+const newDate = new Date();
+const today = newDate.toLocaleString("en-us", { weekday: "long" }).toLowerCase();
+
+if (today === "monday" || today === "tuesday" || today === "wednesday") {
+    banner.classList.add("banner-show")
+    banner.classList.remove("banner-hide");
+} else {
+    banner.classList.add("banner-hide");
+    banner.classList.remove("banner-show");
+}
+
+button.addEventListener("click", () => {
+    banner.classList.add("banner-hide");
+    banner.classList.remove("banner-show");
+})
+
